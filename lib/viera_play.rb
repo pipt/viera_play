53bb9fc6ac344d18db38b5393d81e3adddf5f075
Pip Taylor <pip@evilgeek.co.uk>
2013-03-05 19:14:46 +0000

    Create executable script to invoke player.

--- lib/viera_play.rb
+++ lib/viera_play.rb
@@ -2,8 +2,3 @@ require "viera_play/single_file_server"
 require "viera_play/tv"
 require "viera_play/soapy"
 require "viera_play/player"
-
-VieraPlay::Player.new(
-  :tv_control_url => "http://192.168.0.16:55000/dmr/control_2",
-  :file_path => ARGV.first
-).call
