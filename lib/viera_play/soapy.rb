d4b67bd1b1a7d3918b1ae4d06dd9a48f10368be4
Pip Taylor <pip@evilgeek.co.uk>
2013-03-31 10:45:37 +0100

    Return body of response after sending command.

--- lib/viera_play/soapy.rb
+++ lib/viera_play/soapy.rb
@@ -23,7 +23,12 @@ module VieraPlay
     attr_reader :endpoint, :namespace, :default_request_args
 
     def post(headers, data)
-      Net::HTTP.new(endpoint.host, endpoint.port).post(endpoint.path, data, headers)
+      response = Net::HTTP.new(endpoint.host, endpoint.port).post(
+        endpoint.path,
+        data,
+        headers
+      )
+      response.body
     end
 
     def soap_body(command, args)
