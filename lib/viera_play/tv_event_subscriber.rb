d74e7d6eee377f49b69b38fc8cd575b6a16c2ebd
Pip Taylor <pip@evilgeek.co.uk>
2013-03-31 11:22:16 +0100

    Provide events from TV to callback object.

--- lib/viera_play/tv_event_subscriber.rb
+++ lib/viera_play/tv_event_subscriber.rb
@@ -7,14 +7,14 @@ module VieraPlay
     def initialize
       @server = WEBrick::HTTPServer.new(:Port => 1234)
       Thread.new { @server.start }
-      @server.mount("/notify", NotifyServlet)
+      @server.mount("/notify", NotifyServlet, self)
       sleep 1
       http = Net::HTTP.new("192.168.0.16", "55000")
       request = SubscribeVerb.new("/dmr/event_2")
       request["CALLBACK"] = "<http://#{local_ip}:1234/notify>"
       request["NT"] = "upnp:event"
       p request.each.to_a
-      p http.request(request)
+      p http.request(request).each.to_a
     end
 
     def local_ip
@@ -23,13 +23,25 @@ module VieraPlay
         s.addr.last
       end
     end
+
+    def call(last_change)
+      p last_change
+    end
   end
 end
 
 class NotifyServlet < WEBrick::HTTPServlet::AbstractServlet
+  def initialize(server, callback)
+    super(server)
+    @callback = callback
+  end
+
   def do_NOTIFY(request, response)
     doc = Nokogiri::XML(request.body)
     last_change = Nokogiri::XML(doc.css('LastChange').first.content)
-    puts doc.css('LastChange').first.content
+    callback.call(last_change)
   end
+
+private
+  attr_reader :callback
 end
