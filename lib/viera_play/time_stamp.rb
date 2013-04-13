1a61020fb26812dee4d2d6552a1704f8e648d5b9
Tom Lea <commit@tomlea.co.uk>
2013-04-13 16:58:16 +0100

    Add a TimeStamp class to handle the seek times.

---
+++ lib/viera_play/time_stamp.rb
@@ -0,0 +1,29 @@
+class TimeStamp
+  def self.parse(string)
+    if /\A(-?)([0-9]{2}):([0-9]{2}):([0-9]{2})\Z/.match(string)
+      negative, parts = $1, [$2, $3, $4]
+      parts = parts.map(&:to_i)
+      val = parts[0] * 60**2 + parts[1] * 60 + parts[2]
+      val = -val if negative == '-'
+      new(val)
+    else
+      raise ArgumentError, "Bad timestamp" unless match
+    end
+  end
+
+  def to_s
+    mins, seconds = @val.to_i.abs.divmod(60)
+    hours, mins = mins.divmod(60)
+
+    neg = '-' if @val < 0
+    "%s%02i:%02i:%02i" % [neg, hours, mins, seconds]
+  end
+
+  def initialize(val)
+    @val = val
+  end
+
+  def to_i
+    @val
+  end
+end
---
+++ spec/time_stamp_spec.rb
@@ -0,0 +1,43 @@
+require "viera_play/time_stamp"
+
+describe TimeStamp do
+  describe ".parse" do
+    it "creates a new instance" do
+      TimeStamp.parse('00:00:00').should be_instance_of(TimeStamp)
+    end
+
+    it "parses positive timestamps" do
+      TimeStamp.parse('00:00:00').to_i.should eq 0
+      TimeStamp.parse('00:00:01').to_i.should eq 1
+      TimeStamp.parse('00:01:01').to_i.should eq 61
+      TimeStamp.parse('01:01:01').to_i.should eq 3_661
+    end
+
+    it "parses negative timestamps" do
+      TimeStamp.parse('-00:00:00').to_i.should eq 0
+      TimeStamp.parse('-00:00:01').to_i.should eq(-1)
+      TimeStamp.parse('-00:01:01').to_i.should eq(-61)
+      TimeStamp.parse('-01:01:01').to_i.should eq(-3_661)
+    end
+  end
+
+  describe "#to_s" do
+    it "formats positive timestamps" do
+      TimeStamp.parse('01:01:01').to_s.should eq '01:01:01'
+    end
+
+    it "formats negative timestamps" do
+      TimeStamp.parse('-01:01:01').to_s.should eq '-01:01:01'
+    end
+  end
+
+  describe "#to_i" do
+    it "coerses positive timestamps" do
+      TimeStamp.new(100).to_i.should == 100
+    end
+
+    it "coerses negative timestamps" do
+      TimeStamp.new(-100).to_i.should == -100
+    end
+  end
+end
