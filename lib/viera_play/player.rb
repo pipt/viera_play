bdd7d6a5de26e4feaede4567b91a9812ecb7e7ee
Tom Lea <commit@tomlea.co.uk>
2013-04-14 19:06:15 +0100

    Automatically stop on playback ended.

--- lib/viera_play/player.rb
+++ lib/viera_play/player.rb
@@ -13,62 +13,68 @@ module VieraPlay
       play_and_wait
     end
 
-    private
-    attr_reader :tv, :file_path, :server
+  private
+    attr_reader :tv, :file_path, :server, :http_server_thread
+
+    def exit
+      tv.stop
+      super
+    end
 
     def trap_interrupt
       trap 'INT' do
-        tv.stop
-        server.shutdown
+        trap 'INT', 'DEFAULT'
+        exit
       end
     end
 
+    def redraw
+      info = tv.get_position_info
+
+      win = Curses::Window.new(4, (Curses.cols - 2), Curses.lines/2 - 1, 1)
+      @width = Curses.cols - 4
+      win.clear
+      win.box('|', '-', '+')
+      win.setpos(1,1)
+      win << '#' * (@width * info.position.to_i/(info.duration.to_i + 0.0001))
+      win.setpos(2,1)
+      win.addstr("%s of %s - %20s" % [info.position, info.duration, info.track])
+      win.refresh
+
+      exit if info.duration.to_i.zero?
+    end
+
     def play_and_wait
-      http_server_thread = Thread.new { server.start }
+      @http_server_thread = Thread.new { server.start }
       tv.play_uri(server.url)
       Curses.noecho
       Curses.init_screen
       Curses.stdscr.keypad(true) # enable arrow keys
-
       Curses.mousemask(Curses::BUTTON1_CLICKED)
-      win = Curses::Window.new(4, (Curses.cols - 2), Curses.lines/2 - 1, 1)
-      width = Curses.cols - 4
-      monitor = Thread.start do
-        loop do
-          sleep 1
-          info = tv.get_position_info
-          win.clear
-          win.box('|', '-', '+')
-          win.setpos(1,1)
-          win << '#' * (width * info.position.to_i/(info.duration.to_i + 0.0001))
-          win.setpos(2,1)
-          win.addstr("%s of %s - %20s" % [info.position, info.duration, info.track])
-          win.refresh
-        end
-      end
 
-      while c = Curses.getch
+      Curses.timeout = 1000
+
+      redraw
+      loop do
+        c = Curses.getch
         case c
         when Curses::Key::LEFT
           tv.seek_by(-30)
         when Curses::Key::RIGHT
           tv.seek_by(30)
-        when 'p'
+        when 'p', ' '
           tv.pause
         when 'q'
-          tv.stop
-          server.shutdown
-          break
+          exit
         when Curses::KEY_MOUSE
           if m = Curses.getmouse
-            v = (m.x - 2).to_f / (width) * tv.get_position_info.duration.to_i
+            v = (m.x - 2).to_f / (@width) * tv.get_position_info.duration.to_i
             tv.seek_to(v)
           end
+        when nil
+          redraw
         end
       end
-      http_server_thread.join
-      monitor.kill
-      monitor.join
     end
   end
 end
